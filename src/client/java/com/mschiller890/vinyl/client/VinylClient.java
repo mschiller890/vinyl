a0ea9ed4ba8195b8c3951ecdf3c943c0497066d0
Michal Schiller <mschiller890@gmail.com>
2026-07-13 22:32:44 +0200

    chore: initialize Fabric mod project

---
+++ src/client/java/com/mschiller890/vinyl/client/VinylClient.java
@@ -0,0 +1,10 @@
+package com.mschiller890.vinyl.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VinylClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
